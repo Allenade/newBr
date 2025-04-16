07a2462cbeff75709ecc71e5926d04bb3b8b5036
MAC <mac@Allenade-MacBook-Air.local>
2025-04-16 16:22:15 +0100

    updated

--- src/services/db/supabase/client.ts
+++ src/services/db/supabase/client.ts
@@ -1,20 +1,20 @@
 import { createClient as createSupabaseClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_PROJECT_URL;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error("Missing Supabase environment variables");
+}
 
 // ~ =============================================>
 // ~ ======= Create Supabase Client  -->
 // ~ =============================================>
 export const createClient = () => {
-  // Create a new Supabase client with the environment variables
-  return createSupabaseClient(
-    supabaseUrl || "https://your-project.supabase.co",
-    supabaseAnonKey || "your-anon-key",
-    {
-      auth: {
-        persistSession: true,
-      },
-    }
-  );
+  // Create a new Supabase client with environment variables
+  return createSupabaseClient(supabaseUrl, supabaseAnonKey, {
+    auth: {
+      persistSession: true,
+    },
+  });
 };
--- src/services/db/supabase/ssr.ts
+++ src/services/db/supabase/ssr.ts
@@ -3,7 +3,7 @@ import { createServerClient } from "@supabase/ssr";
 
 // ~ ======= create client -->
 // I would consider this over-engineering but there might be a reason to add more
-// methods to this in the future.
+// methods to this in the future. yes it is to me time to understand it
 class Supabase {
   private readonly url: string;
   private readonly key: string;
@@ -13,7 +13,9 @@ class Supabase {
     this.url =
       process.env.NEXT_PUBLIC_SUPABASE_URL ||
       "https://your-project.supabase.co";
-    this.key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "your-anon-key";
+    this.key =
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ||
+      "yeyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFtd2dxYW11eXd0amhleXFwZ2xnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM4Njg4MjUsImV4cCI6MjA1OTQ0NDgyNX0.viXmNysJLewatBvRasKz4LV2VdYNtq6qi4B1h0cRGNg";
   }
 
   // ~ ======= server client -->
